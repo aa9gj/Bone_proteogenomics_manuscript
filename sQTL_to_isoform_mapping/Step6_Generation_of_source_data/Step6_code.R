07f534ae3a31c3e4be22dd165317b8c054ba30ec
Arby Abood <49687876+aa9gj@users.noreply.github.com>
2023-03-07 10:33:11 -0500

    update paths

--- sQTL_to_isoform_mapping/Step6_Generation_of_source_data/Step6_code.R
+++ sQTL_to_isoform_mapping/Step6_Generation_of_source_data/Step6_code.R
@@ -1,16 +1,16 @@
 # generation of source data for prioritization scheme
 #set up gencode
-gencode_v38 <- as.data.frame(rtracklayer::import('/Users/aa9gj/Documents/BPG_project/Materials_for_paper/gencode.v38.annotation.gtf'))
+gencode_v38 <- as.data.frame(rtracklayer::import('gencode.v38.annotation.gtf'))
 gencode_v38_gene <- subset(gencode_v38, type == "gene")
 gencode_v38_gene <- gencode_v38_gene[, c("gene_id", "gene_type", "gene_name")]
 #keep only protein coding genes
 gencode_v38_gene <- subset(gencode_v38_gene, gene_type == "protein_coding")
-DEA_genes <- read.table("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/tappAS_oct8th2022/tappAS_DEA_Genes.tsv")
+DEA_genes <- read.table("tappAS_DEA_Genes.tsv")
 DEA_genes <- as.data.frame(left_join(DEA_genes, gencode_v38_gene, by = c("V1" = "gene_id")))
 DE_genes <- filter(DEA_genes, DEA_genes$V3 == "DE")
 
 
-DEA_transcript <- fread("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/tappAS_oct8th2022/tappAS_DEA_Transcripts.tsv")
+DEA_transcript <- fread("tappAS_DEA_Transcripts.tsv")
 DEA_transcript <- left_join(DEA_transcript, gencode_v38_gene, by = c("Gene" = "gene_id"))
 gencode_v38_transcript <- subset(gencode_v38, type == "transcript")
 gencode_v38_transcript <- gencode_v38_transcript[, 16:18]
@@ -18,18 +18,18 @@ gencode_v38_transcript <- subset(gencode_v38_transcript, transcript_type == "pro
 DEA_transcript <- as.data.frame(left_join(DEA_transcript, gencode_v38_transcript, by = c("Name/Description" = "transcript_id")))
 DE_transcript <- filter(DEA_transcript, DEA_transcript$`DEA Result` == "DE")
 
-DIU <- fread("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/tappAS_oct8th2022/tappAS_DIUGene_Transcripts.tsv")
+DIU <- fread("tappAS_DIUGene_Transcripts.tsv")
 DIU <- as.data.frame(left_join(DIU, gencode_v38_gene, by = c("Gene Description" = "gene_id")))
 DIU <- subset(DIU, DIU$`DIU Result` == "DIU") 
 
 ## any in IMPC?
-abnormal_bmd <- fread("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/supporting_data/abnormal_bone_mineral_density.tsv", header = T)
+abnormal_bmd <- fread("abnormal_bone_mineral_density.tsv", header = T)
 abnormal_bmd_genes <- as.data.frame(toupper(abnormal_bmd$Gene))
 colnames(abnormal_bmd_genes) <- "gene_name"
-increased_bmd <- fread("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/supporting_data/increased_bone_mineral_density.tsv", header = T)
+increased_bmd <- fread("increased_bone_mineral_density.tsv", header = T)
 increased_bmd_genes <- as.data.frame(toupper(increased_bmd$Gene))
 colnames(increased_bmd_genes) <- "gene_name"
-decreased_bmd <- fread("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/supporting_data/decreased_bone_mineral_density.tsv", header = T)
+decreased_bmd <- fread("decreased_bone_mineral_density.tsv", header = T)
 decreased_bmd_genes <- as.data.frame(toupper(decreased_bmd$Gene))
 colnames(decreased_bmd_genes) <- "gene_name"
 bmd_genes <- full_join(abnormal_bmd_genes, increased_bmd_genes, by = "gene_name")
@@ -39,17 +39,17 @@ colnames(bmd_genes) <- "gene_name"
 bmd_genes$gene_name <- gsub("ZFP", "ZNF", bmd_genes$gene_name)
 
 ## any in bone literature?
-bans_genes <- fread("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/supporting_data/Basel_BANs.csv", header = T)
+bans_genes <- fread("BANs.csv", header = T)
 bans_genes <- bans_genes[BAN == "YES",]
 bans_genes <- as.data.frame(toupper(bans_genes$Gene))
 colnames(bans_genes) <- "gene_name"
 bans_genes$gene_name <- gsub("ZFP", "ZNF", bans_genes$gene_name)
 
-eqtl_genes <- fread("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/supporting_data/Basel_eqtl.txt", header = T)
+eqtl_genes <- fread("eqtl.txt", header = T)
 eqtl_genes <- as.data.frame(unique(eqtl_genes$`Gene Name`))
 colnames(eqtl_genes) <- "gene_name"
 
-mono_genes <- fread("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/supporting_data/bone_genes_olivia", header = F)
+mono_genes <- fread("bone_disease_genes", header = F)
 mono_genes <- as.data.frame(toupper(mono_genes$V1))
 colnames(mono_genes) <- "gene_name"
 mono_genes$gene_name <- gsub("ZFP", "ZNF", mono_genes$gene_name)
@@ -73,4 +73,3 @@ source_data_tableS$eQTL_evidence <- ifelse(source_data_tableS$gene_name %in% eqt
 source_data_tableS$monogenic_evidence <- ifelse(source_data_tableS$gene_name %in% mono_genes$gene_name, 1, 0)
 source_data_tableS <- inner_join(source_data_tableS, gene_tissue_count_pc, by = c("gene_name" = "Var1"))
 source_data_tableS <- inner_join(source_data_tableS, by_event_pc_source, by = c("event_id" = "Var1"))
-write.table(source_data_tableS, "/Users/aa9gj/Documents/BPG_project/Materials_for_paper/source_data_feb6", quote = F, row.names = F)
