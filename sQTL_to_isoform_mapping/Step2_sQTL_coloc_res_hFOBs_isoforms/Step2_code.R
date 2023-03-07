644d6b06b64870434e4480c0aa33b9fa6262266d
Arby Abood <49687876+aa9gj@users.noreply.github.com>
2023-03-07 10:23:30 -0500

    updated paths

--- sQTL_to_isoform_mapping/Step2_sQTL_coloc_res_hFOBs_isoforms/Step2_code.R
+++ sQTL_to_isoform_mapping/Step2_sQTL_coloc_res_hFOBs_isoforms/Step2_code.R
@@ -8,7 +8,7 @@
 
 # get sQTLs that are in hFOBs 
 ##read in all sig events from all tissues and change them into a full dataframe
-setwd("/Users/aa9gj/Documents/BPG_project/coloc_results_full/coloc_annot_results/")
+setwd("coloc_annot_results/")
 my_files <- fread('my_files',header=FALSE)
 my_data <- list()
 for (i in seq_along(my_files$V1)) {
@@ -28,7 +28,7 @@ coloc_sqtl_sig_pc <- filter(coloc_sqtl_sig, gene_type == 'protein_coding')#prote
 dim(coloc_sqtl_sig_pc)
 str(coloc_sqtl_sig_pc)
 length(unique(coloc_sqtl_sig_pc$event_id)) #1863 events with coloc sqtl in protein-coding genes
-write.table(coloc_sqtl_sig_pc, file = "/Users/aa9gj/Documents/BPG_project/coloc_results_full/coloc_annot_results/coloc_sig_pc_events", quote = F, row.names = F)
+write.table(coloc_sqtl_sig_pc, file = "coloc_sig_pc_events", quote = F, row.names = F)
 
 #Group by gene, tissue, event
 #for pc genes only
@@ -38,13 +38,11 @@ length(unique(by_gene_pc$gene_name))
 
 gene_tissue_count <- table(by_gene_pc$gene_name)
 gene_tissue_count_pc <- data.frame(gene_tissue_count)
-write.table(gene_tissue_count_pc, "/Users/aa9gj/Documents/BPG_project/Materials_for_paper/Tables/S3", quote = F, row.names = F)
 
 by_event_pc <- coloc_sqtl_sig_pc %>% group_by(event_id, tissue) %>% dplyr::summarise(n = n())
 length(unique(by_event_pc$event_id))
 by_tissue_pc <- coloc_sqtl_sig_pc %>% group_by(tissue) %>% dplyr::summarise(n = n())
 length(unique(by_tissue_pc$tissue))
-#write.table(by_tissue_pc, "/Users/aa9gj/Documents/BPG_project/Materials_for_paper/Tables/S2", quote = F, row.names = F)
 
 # for all kinds of genes
 by_gene <- coloc_sqtl_sig %>% group_by(gene_name, tissue) %>% dplyr::summarise(n = n())
@@ -55,7 +53,6 @@ by_event <- coloc_sqtl_sig %>% group_by(event_id, tissue) %>% dplyr::summarise(n
 length(unique(by_event$event_id))
 by_tissue <- coloc_sqtl_sig %>% group_by(tissue) %>% dplyr::summarise(n = n())
 length(unique(by_tissue$tissue))
-#write.table(by_tissue, "/Users/aa9gj/Documents/BPG_project/Materials_for_paper/Tables/S1", quote = F, row.names = F)
 
 by_event_summary <- coloc_sqtl_sig_pc %>% group_by(event_id) %>% dplyr::summarise(n = n())
 by_event_summary <- do.call("data.frame", by_event_summary) # flatten
@@ -77,7 +74,7 @@ events$end <- as.numeric(events$end) -1
 events_gr <- makeGRangesFromDataFrame(events, keep.extra.columns = TRUE,
                                       start.field = "start", end.field = "end", seqnames.field = "seq", ignore.strand = T)
 
-sqanti_gtf <- read_format("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/aug2022_reanalysis/SQANTI3_results_full_corrected_chr_only.gtf")
+sqanti_gtf <- read_format("SQANTI3_results_full_corrected_chr_only.gtf")
 sqanti_gtf_intron <- construct_introns(sqanti_gtf, update = TRUE)[]
 sqanti_gtf_intron <- plyranges::filter(sqanti_gtf_intron, feature == "intron" | feature == "transcript")
 sqanti_gtf_intron <- as.data.frame(sqanti_gtf_intron)
@@ -110,7 +107,7 @@ length(unique(exact_overlap$transcript_id))#2349
 genes_in_hfobs_and_coloc <- as.data.frame(unique(exact_overlap$gene_name))
 colnames(genes_in_hfobs_and_coloc) <- "gene_name"
 length(unique(events$gene_name))
-write.table(genes_in_hfobs_and_coloc, "/Users/aa9gj/Documents/BPG_project/Materials_for_paper/genes_in_hfobs_coloc", quote = F, row.names = F, col.names = F)
+write.table(genes_in_hfobs_and_coloc, "genes_in_hfobs_coloc", quote = F, row.names = F, col.names = F)
 #make sure to add the +1 and -1 to get the events back to their original coordinates like in GTEx
 exact_overlap$event_id <- paste0(exact_overlap$seqnames, "_", exact_overlap$start-1, "_", exact_overlap$end+1, "_", exact_overlap$gene_name)
 length(unique(exact_overlap$event_id))#we end up with 836 unique events in coloc/hfobs/filter/exactmatch
@@ -132,8 +129,8 @@ nrow(filter(test3, status == "novel")) #103 events explained by novel isoforms o
 ## Distribution of those genes per lead GWAS SNP, put it in a supplementary table
 # create a genomic ranges for those genes 
 dist_genes <- inner_join(genes_in_hfobs_and_coloc, gencodev38, by = "gene_name")
-morris_lead_snps <- fread("/Users/aa9gj/Documents/BPG_project/morris_lead_snps_hg19.txt")
-ch <- import.chain("/Users/aa9gj/Documents/BPG_project/hg19ToHg38.over.chain")
+morris_lead_snps <- fread("morris_lead_snps_hg19.txt")
+ch <- import.chain("hg19ToHg38.over.chain")
 morris_overlift <- makeGRangesFromDataFrame(morris_lead_snps, keep.extra.columns = TRUE, start.field = "V4", end.field = "V4", seqnames.field = "V3", na.rm = T)
 seqlevelsStyle(morris_overlift) = "UCSC"  # necessary
 morris_overlift = liftOver(morris_overlift, ch)
@@ -157,11 +154,11 @@ dist_sgenes <- function(x,y) {
 }
 test_dist1 <- as.data.frame(dist_sgenes(dist_genes_gr, morris_overlift_gr))
 test_dist2 <- as.data.frame(dist_sgenes(morris_overlift_gr, dist_genes_gr))
-write.table(test_dist1, "/Users/aa9gj/Documents/BPG_project/overlap_morris_sgenes1", quote = F, row.names = F)
-write.table(test_dist2, "/Users/aa9gj/Documents/BPG_project/overlap_morris_sgenes2", quote = F, row.names = F)
+write.table(test_dist1, "overlap_morris_sgenes1", quote = F, row.names = F)
+write.table(test_dist2, "overlap_morris_sgenes2", quote = F, row.names = F)
 length(unique(test_dist2$snp_internal_id))
 length(unique(test_dist1$gene_name))
-gwas_sgene_dist <- fread("/Users/aa9gj/Documents/BPG_project/gwas_sgenes_dist.txt")
+gwas_sgene_dist <- fread("gwas_sgenes_dist.txt")
 gwas_sgene_dist_count <- gwas_sgene_dist %>% group_by(snp_internal_id) %>% dplyr::summarise(n = dplyr::n())
 one_sgene <- filter(gwas_sgene_dist_count, n == 1)
 more_sgene <- filter(gwas_sgene_dist_count, n > 1)
