20f87debd7fc026911aac298634c3c1a519c3679
Arby Abood <49687876+aa9gj@users.noreply.github.com>
2023-03-07 10:30:58 -0500

    update paths

--- sQTL_to_isoform_mapping/Step5_tappAS_analysis/Step5_code.R
+++ sQTL_to_isoform_mapping/Step5_tappAS_analysis/Step5_code.R
@@ -6,18 +6,18 @@
 ###########################################################################
 ###########################################################################
 #set up gencode
-gencode_v38 <- as.data.frame(rtracklayer::import('/Users/aa9gj/Documents/BPG_project/Materials_for_paper/gencode.v38.annotation.gtf'))
+gencode_v38 <- as.data.frame(rtracklayer::import('gencode.v38.annotation.gtf'))
 gencode_v38_gene <- subset(gencode_v38, type == "gene")
 gencode_v38_gene <- gencode_v38_gene[, c("gene_id", "gene_type", "gene_name")]
 #keep only protein coding genes
 gencode_v38_gene <- subset(gencode_v38_gene, gene_type == "protein_coding")
-DEA_genes <- read.table("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/tappAS_oct8th2022/tappAS_DEA_Genes.tsv")
+DEA_genes <- read.table("tappAS_DEA_Genes.tsv")
 DEA_genes <- as.data.frame(left_join(DEA_genes, gencode_v38_gene, by = c("V1" = "gene_id")))
 nrow(DEA_genes) #2034 DE genes in total
 DE_genes <- filter(DEA_genes, DEA_genes$V3 == "DE")
 
 
-DEA_transcript <- fread("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/tappAS_oct8th2022/tappAS_DEA_Transcripts.tsv")
+DEA_transcript <- fread("tappAS_DEA_Transcripts.tsv")
 DEA_transcript <- left_join(DEA_transcript, gencode_v38_gene, by = c("Gene" = "gene_id"))
 gencode_v38_transcript <- subset(gencode_v38, type == "transcript")
 gencode_v38_transcript <- gencode_v38_transcript[, 16:18]
@@ -26,7 +26,7 @@ DEA_transcript <- as.data.frame(left_join(DEA_transcript, gencode_v38_transcript
 DE_transcript <- filter(DEA_transcript, DEA_transcript$`DEA Result` == "DE")
 
 
-DIU <- fread("/Users/aa9gj/Documents/BPG_project/Materials_for_paper/tappAS_oct8th2022/tappAS_DIUGene_Transcripts.tsv")
+DIU <- fread("tappAS_DIUGene_Transcripts.tsv")
 DIU <- as.data.frame(left_join(DIU, gencode_v38_gene, by = c("Gene Description" = "gene_id")))
 DIU <- subset(DIU, DIU$`DIU Result` == "DIU") 
 DIU_genes <- as.data.frame(DIU$gene_name)
