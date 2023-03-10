27924976cabcbea2cf3bf4916cc26b4ed38ff491
Arby Abood <49687876+aa9gj@users.noreply.github.com>
2023-03-10 08:17:42 -0500

    Update Step1_code.R

--- Reference_transcriptome_generation/Step1_Long-read_RNAseq_filtering_in_hFOBs/Step1_code.R
+++ Reference_transcriptome_generation/Step1_Long-read_RNAseq_filtering_in_hFOBs/Step1_code.R
@@ -6,7 +6,7 @@
 ###########################################################################
 ###########################################################################
 
-# Where are these files? 
+# Where do I get these files to run this code? 
 # Download the following files from the Zenodo repo (counts_cupcake.mapped_fl_count.txt; phenotypes.txt; SQANTI3_results_full_classification.txt)
 # Download GENCODE v38 gtf file from GENCODE
 
